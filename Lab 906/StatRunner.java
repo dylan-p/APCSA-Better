663ce8fa27e15ef5adde551ccd48ba611c0660ea
dylan-p <31372610+dylan-p@users.noreply.github.com>
2018-09-06 10:35:32 -0700

    Add files via upload

---
+++ Lab 906/StatRunner.java
@@ -0,0 +1,16 @@
+
+/**
+ * Lab 906::Find Mean Median and Mode
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class StatRunner{
+    
+    
+    public static void main(){
+        StatTester st = new StatTester();
+        st.loadArray();
+        st.printArray();
+    }
+}
